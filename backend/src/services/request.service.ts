8514caa93c3921eb91417d132dc281c2fef15a3a
ceeto209 <vasisrael98@outlook.com>
2025-07-12 12:36:47 -0700

    Update request service

--- backend/src/services/request.service.ts
+++ backend/src/services/request.service.ts
@@ -1,5 +1,5 @@
 import { AppDataSource } from '../config/data-source';
-import { Request, RequestType, RequestStatus } from '../models/Request.ts';
+import { Request, RequestType, RequestStatus } from '../models/Request';
 import { User } from '../models/User';
 
 export class RequestService {
@@ -16,7 +16,7 @@ export class RequestService {
 			throw new Error('Inmate not found');
 		}
 
-		const request = await this.requestRepository.create({
+		const request = this.requestRepository.create({
 			...data,
 			requestStatus: RequestStatus.PENDING
 		});
@@ -26,11 +26,73 @@ export class RequestService {
 		return savedRequest;
 	}
 
+	async getRequestById(id: number): Promise<Request> {
+		const request = await this.requestRepository.findOne({
+			where: { id },
+			relations: ['inmate', 'reviwer']
+		});
+
+		if (!request) {
+			throw new Error('Request not found.');
+		}
+
+		return request;
+	}
+
 	async getInmateRequest(inmateId: number): Promise<Request[]> {
 		return await this.requestRepository.find({
 			where: { inmateId },
 			relations: ['reviewer'],
 			order: { createdAt: 'DESC' }
 		});
+	}
+
+	async getAllRequests(status?: RequestStatus): Promise<Request[]> {
+		const where = status ? { requestStatus: status } : {};
+
+		return await this.requestRepository.find({
+			where,
+			relations: ['inmate', 'reviewer'],
+			order: { createdAt: 'DESC' }
+		});
+	}
+
+	async reviewRequest(
+		requestId: number,
+		reviewerId: number,
+		status: RequestStatus,
+		reviewNotes?: string
+	): Promise<Request> {
+		const request = await this.getRequestById(requestId);
+		const reviewer = await this.userRepository.findOne({ where: { id: reviewerId } });
+
+		if (!reviewer) {
+			throw new Error('Reviwer not found');
+		}
+
+		if (request.requestStatus !== RequestStatus.PENDING && request.requestStatus !== RequestStatus.DISPUTED) {
+			throw new Error('Request cannot be reviewed in its current state');
+		}
+
+		request.requestStatus = status;
+		request.reviewedBy = reviewerId;
+		request.reviewNotes = reviewNotes;
+
+		const updatedRequest = await this.requestRepository.save(request);
+		return updatedRequest;
+	}
+
+	async disputedRequest(requestId: number, disputeReason: string): Promise<Request> {
+		const request = await this.getRequestById(requestId);
+
+		if (request.requestStatus !== RequestStatus.DENIED) {
+			throw new Error('Only denied request can be disputed');
+		}
+
+		request.requestStatus = RequestStatus.DISPUTED;
+		request.disputeReason = disputeReason;
+
+		const updateRequest = await this.requestRepository.save(request);
+		return updateRequest;
 	}
 }
