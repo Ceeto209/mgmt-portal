ca2c38b66a0875e8d1f03f1e88a6d0980f50a237
ceeto209 <vasisrael98@outlook.com>
2025-07-14 15:58:10 -0700

    Update request routes

--- backend/src/routes/request.routes.ts
+++ backend/src/routes/request.routes.ts
@@ -3,18 +3,20 @@ import { RequestController } from '../controllers/request.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 import { RequestService } from '../services/request.service';
 
-const router = Router();
-const requestService = new RequestService();
-const requestController = new RequestController(requestService);
+export const createRequestRoutes = () => {
+	const router = Router();
+	const requestService = new RequestService();
+	const requestController = new RequestController(requestService);
 
-//inmate routes
-router.post('/', authMiddleware(['inmate']), requestController.createRequest);
-router.get('/my-requests', authMiddleware(['inmate']), requestController.getInmateRequests);
-router.get('/:id', authMiddleware(['inmate', 'guard', 'admin']), requestController.getRequest);
-router.post('/:id/dispute', authMiddleware(['inmate']), requestController.disputeRequest);
+	//inmate routes
+	router.post('/', authMiddleware(['inmate']), requestController.createRequest);
+	router.get('/my-requests', authMiddleware(['inmate']), requestController.getInmateRequests);
+	router.get('/:id', authMiddleware(['inmate', 'guard', 'admin']), requestController.getRequest);
+	router.post('/:id/dispute', authMiddleware(['inmate']), requestController.disputeRequest);
 
-// admin, gaurd routes
-router.get('/', authMiddleware(['guard', 'admin']), requestController.getAllRequests);
-router.post('/:id/review', authMiddleware(['guard', 'admin']), requestController.reviewRequest);
+	// admin, gaurd routes
+	router.get('/', authMiddleware(['guard', 'admin']), requestController.getAllRequests);
+	router.post('/:id/review', authMiddleware(['guard', 'admin']), requestController.reviewRequest);
 
-export default router;
+	return router;
+}
