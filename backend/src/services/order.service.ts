84f9d7ef8d007a36671f7e15e4e86a9a9b383c79
ceeto209 <vasisrael98@outlook.com>
2025-07-14 15:36:45 -0700

    Add order service, not finished

---
+++ backend/src/services/order.service.ts
@@ -0,0 +1,28 @@
+import { AppDataSource } from '../config/data-source';
+import { Order, OrderStatus, OrderType } from '../models/Order';
+import { User } from '../models/User';
+
+export class OrderService {
+	private orderRepository = AppDataSource.getRepository(Order);
+	private userRepository = AppDataSource.getRepository(User);
+
+	async createOrder(data: {
+		inmateId: number;
+		orderItem: OrderType;
+	}): Promise<Order> {
+		const inmate = await this.userRepository.findOne({ where: { id: data.inmateId } });
+		if (!inmate) {
+			throw new Error('Inmate not found');
+		}
+
+		const order = this.orderRepository.create({
+			...data,
+			oderStatus: OrderStatus.PENDING
+		});
+
+		const savedOrder = await this.orderRepository.save(order);
+
+		return savedOrder;
+	}
+
+}
